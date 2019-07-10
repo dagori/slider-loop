56e58c64e5f13682b1faa1f18387f9f0682135da
dagori <mariyav@protonmail.com>
2019-07-10 15:04:03 +0300

    init

--- js/script.js
+++ js/script.js
@@ -5,47 +5,94 @@ var next = slider.querySelector('.slider__button--next');
 var container = slider.querySelector('.slider__container');
 
 var step = parseInt(getComputedStyle(slider).width);
-console.log(step);
+var shift = 1;
 var pos = 0;
+const transition = 'transform .5s linear';
+var length;
 
 function createClone() {
-  pos-=step;
+  pos = -step;
   container.style.transform = `translateX(${pos}px)`;
   var prevSlide = container.children[0].cloneNode(true);
   var nextSlide = container.children[container.children.length - 1].cloneNode(true);
   container.insertBefore(nextSlide, container.children[0]);
   container.appendChild(prevSlide);
+  length = container.children.length;
+  console.log(length);
 }
 createClone();
 
-prev.onclick = function() {
-  if(!pos) {
-    pos = -container.offsetWidth + step * 2;
+function movePrev() {
+  if(!shift) {
+    shift = length - 2;
+    pos = -(shift * step);
     container.style.transition = 'null';
     container.style.transform = `translateX(${pos}px)`;
   }
   requestAnimationFrame(function(){
     requestAnimationFrame(function(){
-      container.style.transition = 'transform 1s';
-      pos+=step;
+      container.style.transition = transition;
+      shift--;
+      pos = -(shift * step);
       container.style.transform = `translateX(${pos}px)`;
-      console.log(pos);
+      console.log(`pos:${pos}, shift:${shift}`);
     });
   });
 }
 
-next.onclick = function() {
-  if(pos === -container.offsetWidth + step) {
+function moveNext() {
+  if(shift === length - 1 || shift > length - 1) {
+    shift = 1;
     pos = -step;
     container.style.transition = 'null';
     container.style.transform = `translateX(${pos}px)`;
   }
   requestAnimationFrame(function(){
     requestAnimationFrame(function(){
-      container.style.transition = 'transform 1s';
-      pos-=step;
+      container.style.transition = transition;
+      shift++;
+      pos = -(shift * step);
       container.style.transform = `translateX(${pos}px)`;
-      console.log(pos);
+      console.log(`pos:${pos}, shift:${shift}`);
     });
   });
 }
+var timer;
+
+function autoplay() {
+  timer = setTimeout(function() {
+    moveNext();
+    autoplay();
+  }, 4000);
+}
+
+autoplay();
+
+function resetAutoplay() {
+  clearTimeout(timer);
+}
+
+document.addEventListener('visibilitychange', function() {
+  console.log('state:' + document.visibilityState);
+  if(document.visibilityState === 'hidden') {
+    resetAutoplay();
+  } else {
+    pos = -step;
+    shift = 1;
+    container.style.transition = '';
+    container.style.transform = `translateX(${pos}px)`;
+    autoplay();
+  }
+});
+
+prev.addEventListener('click', function() {
+  resetAutoplay();
+  movePrev();
+  autoplay();
+});
+
+next.addEventListener('click', function() {
+  resetAutoplay();
+  moveNext();
+  autoplay();
+});
